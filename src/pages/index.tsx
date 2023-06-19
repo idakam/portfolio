d35527062993c7c2b3f7fc3cb7f1673164361160
ida <ida.kamali10@gmail.com>
2023-06-19 14:15:28 -0400

    header logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,14 @@ import Head from "next/head";
 import Main from "../components/Main";
 import Navbar from "../components/Navbar";
 import Beatdriver from "./projects/beatdriver";
+// import logo from "../../public/assets/logo.png"
 
 export default function Home() {
   return (
     <div>
       <Head>
         <title>EYEDUH</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/assets/logo.png" />
       </Head>
 
       <Main />
