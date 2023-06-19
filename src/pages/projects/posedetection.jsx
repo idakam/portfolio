85e5a969aafce35335ece46cd09a737dcd699aa3
ida <ida.kamali10@gmail.com>
2023-06-19 13:46:55 -0400

    props

---
+++ src/pages/projects/posedetection.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import Project from "../../components/Project";
+import projectImg from "../../../public/assets/projects/beatdriver.png";
+
+const PoseDetection = () => {
+  return (
+    <Project
+      type="Projects"
+      title="3D Pose Detection"
+      projectImg={projectImg}
+      p1="Coming soon.."
+      github="https://github.com/idakam/interactiveRoom"
+    />
+  );
+};
+
+export default PoseDetection;
