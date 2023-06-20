4242cdeb8d5b9f575a75610bd0a87f94de4ecd18
ida <ida.kamali10@gmail.com>
2023-06-20 12:59:13 -0400

    hydration issue fix

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -12,7 +12,7 @@ const Project = ({ type, title, projectImg, p1, p2, deployed, github }) => {
           </p>
           <h1 className="py-4 uppercase">{title}</h1>
           <div className="flex flex-col">
-            <p className="py-2  tracking-widest">{p1}</p>
+            <div className="py-2  tracking-widest">{p1}</div>
             <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group opacity-80 hover:bg-gradient-to-r from-[#0b316e] to-[#3d69e4]">
               <Image
                 className="rounded-xl group-hover:opacity-10"
