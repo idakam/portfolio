6246dd111df67f6e4d0c4ebde3edcf4c6b23e022
ida <ida.kamali10@gmail.com>
2023-06-20 12:53:03 -0400

    project descriptions

--- src/pages/projects/beatdriver.jsx
+++ src/pages/projects/beatdriver.jsx
@@ -8,8 +8,18 @@ const Beatdriver = () => {
       type="Projects"
       title="Beatdriver"
       projectImg={projectImg}
-      p1="Beatdriver is a beginner friendly music creation tool that you can use right in the browser. Users can create beats with built-in sounds and effects, upload their own samples, and save and record their project to share with anyone!"
-      p2="Using Web Audio Api and TUNAJs, an audio effects library, Beatdriver uses modular routing system to create a mixer with 4 different effects: Chorus, Phaser, Tremolo and Moog. As a user, you can also browse a Discover page showcasing all saved creations, complete with visual previews of board states."
+      p1={
+        <p>
+          Beatdriver is a beginner friendly music creation tool using{" "}
+          <span className="font-bold">
+            Next.Js, Firebase, JavaScript, React, Web Audio API
+          </span>{" "}
+          that can be used right in the browser. Users can create beats with
+          built-in sounds and effects, upload their own samples, and save and
+          record their project to share with anyone!
+        </p>
+      }
+      p2="Using Web Audio Api and TUNAJs, an audio effects library, Beatdriver uses modular routing system to create a mixer with 4 different effects: Chorus, Phaser, Tremolo and Moog. User's can save their creations and browse through the Discover page showcasing other user's saved creations."
       deployed="https://beatdriver.vercel.app"
       github="https://github.com/FC-TEAM-VISA/beatdriver"
     />
