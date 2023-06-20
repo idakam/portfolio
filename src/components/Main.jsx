9c8d5fb0ad9c81c171118112c1bae20565df7bef
ida <ida.kamali10@gmail.com>
2023-06-19 23:45:12 -0400

    update head

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,119 +1,127 @@
 import Image from "next/image";
 import React from "react";
+import dynamic from "next/dynamic";
 import head from "../../public/assets/headlogo.png";
+// import ModelViewer from "../components/Three/ModelViewer";
+const ModelViewer = dynamic(import("../components/Three/ModelViewer"), {
+  ssr: false,
+});
 
 const Main = () => {
   return (
-    <div className="fixed mr-4 text-justify items-center justify-center ">
-      <p className="z-0 uppercase tracking-widest text-black overflow-y-hidden sm:text-sm md:text-base lg:text-lg xl:text-xl 2xl:text-2xl">
-        hello my name is ida <span className="text-red-700">(eye-duh)</span> and
-        welcome to my website hello my name is ida{" "}
-        <span className="text-red-700">(eye-duh)</span> and welcome to my
-        website hello my name is ida{" "}
-        <span className="text-red-700">(eye-duh)</span> and welcome to my
-        website hello my name is ida{" "}
-        <span className="text-red-700">(eye-duh)</span> and welcome to my
-        website hello my name is ida{" "}
-        <span className="text-red-700">(eye-duh)</span> and welcome to my
-        website hello my name is ida{" "}
-        <span className="text-red-700">(eye-duh)</span> and welcome to my
-        website hello my name is ida{" "}
-        <span className="text-red-700">(eye-duh)</span> and welcome to my
-        website hello my name is ida{" "}
-        <span className="text-red-700">(eye-duh)</span> and welcome to my
-        website hello my name is ida{" "}
-        <span className="text-red-700">(eye-duh)</span> and welcome to my
-        website hello my name is ida{" "}
-        <span className="text-red-700">(eye-duh)</span> and welcome to my
-        website hello my name is ida{" "}
-        <span className="text-red-700">(eye-duh)</span> and welcome to my
-        website hello my name is ida{" "}
-        <span className="text-red-700">(eye-duh)</span> and welcome to my
-        website hello my name is ida{" "}
-        <span className="text-red-700">(eye-duh)</span> and welcome to my
-        website hello my name is ida{" "}
-        <span className="text-red-700">(eye-duh)</span> and welcome to my
-        website hello my name is ida{" "}
-        <span className="text-red-700">(eye-duh)</span> and welcome to my
-        website hello my name is ida{" "}
-        <span className="text-red-700">(eye-duh)</span> and welcome to my
-        website hello my name is ida{" "}
-        <span className="text-red-700">(eye-duh)</span> and welcome to my
-        website hello my name is ida{" "}
-        <span className="text-red-700">(eye-duh)</span> and welcome to my
-        website hello my name is ida{" "}
-        <span className="text-red-700">(eye-duh)</span> and welcome to my
-        website hello my name is ida{" "}
-        <span className="text-red-700">(eye-duh)</span> and welcome to my
-        website hello my name is ida{" "}
-        <span className="text-red-700">(eye-duh)</span> and welcome to my
-        website hello my name is ida{" "}
-        <span className="text-red-700">(eye-duh)</span> and welcome to my
-        website hello my name is ida{" "}
-        <span className="text-red-700">(eye-duh)</span> and welcome to my
-        website hello my name is ida{" "}
-        <span className="text-red-700">(eye-duh)</span> and welcome to my
-        website hello my name is ida{" "}
-        <span className="text-red-700">(eye-duh)</span> and welcome to my
-        website hello my name is ida{" "}
-        <span className="text-red-700">(eye-duh)</span> and welcome to my
-        website hello my name is ida{" "}
-        <span className="text-red-700">(eye-duh)</span> and welcome to my
-        website hello my name is ida{" "}
-        <span className="text-red-700">(eye-duh)</span> and welcome to my
-        website hello my name is ida{" "}
-        <span className="text-red-700">(eye-duh)</span> and welcome to my
-        website hello my name is ida{" "}
-        <span className="text-red-700">(eye-duh)</span> and welcome to my
-        website hello my name is ida{" "}
-        <span className="text-red-700">(eye-duh)</span> and welcome to my
-        website hello my name is ida{" "}
-        <span className="text-red-700">(eye-duh)</span> and welcome to my
-        website hello my name is ida{" "}
-        <span className="text-red-700">(eye-duh)</span> and welcome to my
-        website hello my name is ida{" "}
-        <span className="text-red-700">(eye-duh)</span> and welcome to my
-        website hello my name is ida{" "}
-        <span className="text-red-700">(eye-duh)</span> and welcome to my
-        website hello my name is ida{" "}
-        <span className="text-red-700">(eye-duh)</span> and welcome to my
-        website hello my name is ida{" "}
-        <span className="text-red-700">(eye-duh)</span> and welcome to my
-        website hello my name is ida{" "}
-        <span className="text-red-700">(eye-duh)</span> and welcome to my
-        website hello my name is ida{" "}
-        <span className="text-red-700">(eye-duh)</span> and welcome to my
-        website hello my name is ida{" "}
-        <span className="text-red-700">(eye-duh)</span> and welcome to my
-        website hello my name is ida{" "}
-        <span className="text-red-700">(eye-duh)</span> and welcome to my
-        website hello my name is ida{" "}
-        <span className="text-red-700">(eye-duh)</span> and welcome to my
-        website hello my name is ida{" "}
-        <span className="text-red-700">(eye-duh)</span> and welcome to my
-        website hello my name is ida{" "}
-        <span className="text-red-700">(eye-duh)</span> and welcome to my
-        website hello my name is ida{" "}
-        <span className="text-red-700">(eye-duh)</span> and welcome to my
-        website hello my name is ida{" "}
-        <span className="text-red-700">(eye-duh)</span> and welcome to my
-        website hello my name is ida{" "}
-        <span className="text-red-700">(eye-duh)</span> and welcome to my
-        website hello my name is ida{" "}
-        <span className="text-red-700">(eye-duh)</span> and welcome to my
-        website hello my name is ida{" "}
-        <span className="text-red-700">(eye-duh)</span> and welcome to my
-        website hello my name is ida{" "}
-        <span className="text-red-700">(eye-duh)</span> and welcome to my
-        website hello my name is ida{" "}
-        <span className="text-red-700">(eye-duh)</span> and welcome to my
-        website hello my name is ida{" "}
-        <span className="text-red-700">(eye-duh)</span> and welcome to my
-        website hello my name is ida{" "}
-        <span className="text-red-700">(eye-duh)</span> and welcome to my
-        website
-      </p>
-    </div>
+    <>
+      {/* <ModelViewer modelPath={"/assets/faceblend.glb"} /> */}
+      <div className="fixed mr-4 text-justify items-center justify-center ">
+        <p className="z-0 uppercase tracking-widest text-black overflow-y-hidden sm:text-sm md:text-base lg:text-lg xl:text-xl 2xl:text-2xl">
+          hello my name is ida <span className="text-red-700">(eye-duh)</span>{" "}
+          and welcome to my website hello my name is ida{" "}
+          <span className="text-red-700">(eye-duh)</span> and welcome to my
+          website hello my name is ida{" "}
+          <span className="text-red-700">(eye-duh)</span> and welcome to my
+          website hello my name is ida{" "}
+          <span className="text-red-700">(eye-duh)</span> and welcome to my
+          website hello my name is ida{" "}
+          <span className="text-red-700">(eye-duh)</span> and welcome to my
+          website hello my name is ida{" "}
+          <span className="text-red-700">(eye-duh)</span> and welcome to my
+          website hello my name is ida{" "}
+          <span className="text-red-700">(eye-duh)</span> and welcome to my
+          website hello my name is ida{" "}
+          <span className="text-red-700">(eye-duh)</span> and welcome to my
+          website hello my name is ida{" "}
+          <span className="text-red-700">(eye-duh)</span> and welcome to my
+          website hello my name is ida{" "}
+          <span className="text-red-700">(eye-duh)</span> and welcome to my
+          website hello my name is ida{" "}
+          <span className="text-red-700">(eye-duh)</span> and welcome to my
+          website hello my name is ida{" "}
+          <span className="text-red-700">(eye-duh)</span> and welcome to my
+          website hello my name is ida{" "}
+          <span className="text-red-700">(eye-duh)</span> and welcome to my
+          website hello my name is ida{" "}
+          <span className="text-red-700">(eye-duh)</span> and welcome to my
+          website hello my name is ida{" "}
+          <span className="text-red-700">(eye-duh)</span> and welcome to my
+          website hello my name is ida{" "}
+          <span className="text-red-700">(eye-duh)</span> and welcome to my
+          website hello my name is ida{" "}
+          <span className="text-red-700">(eye-duh)</span> and welcome to my
+          website hello my name is ida{" "}
+          <span className="text-red-700">(eye-duh)</span> and welcome to my
+          website hello my name is ida{" "}
+          <span className="text-red-700">(eye-duh)</span> and welcome to my
+          website hello my name is ida{" "}
+          <span className="text-red-700">(eye-duh)</span> and welcome to my
+          website hello my name is ida{" "}
+          <span className="text-red-700">(eye-duh)</span> and welcome to my
+          website hello my name is ida{" "}
+          <span className="text-red-700">(eye-duh)</span> and welcome to my
+          website hello my name is ida{" "}
+          <span className="text-red-700">(eye-duh)</span> and welcome to my
+          website hello my name is ida{" "}
+          <span className="text-red-700">(eye-duh)</span> and welcome to my
+          website hello my name is ida{" "}
+          <span className="text-red-700">(eye-duh)</span> and welcome to my
+          website hello my name is ida{" "}
+          <span className="text-red-700">(eye-duh)</span> and welcome to my
+          website hello my name is ida{" "}
+          <span className="text-red-700">(eye-duh)</span> and welcome to my
+          website hello my name is ida{" "}
+          <span className="text-red-700">(eye-duh)</span> and welcome to my
+          website hello my name is ida{" "}
+          <span className="text-red-700">(eye-duh)</span> and welcome to my
+          website hello my name is ida{" "}
+          <span className="text-red-700">(eye-duh)</span> and welcome to my
+          website hello my name is ida{" "}
+          <span className="text-red-700">(eye-duh)</span> and welcome to my
+          website hello my name is ida{" "}
+          <span className="text-red-700">(eye-duh)</span> and welcome to my
+          website hello my name is ida{" "}
+          <span className="text-red-700">(eye-duh)</span> and welcome to my
+          website hello my name is ida{" "}
+          <span className="text-red-700">(eye-duh)</span> and welcome to my
+          website hello my name is ida{" "}
+          <span className="text-red-700">(eye-duh)</span> and welcome to my
+          website hello my name is ida{" "}
+          <span className="text-red-700">(eye-duh)</span> and welcome to my
+          website hello my name is ida{" "}
+          <span className="text-red-700">(eye-duh)</span> and welcome to my
+          website hello my name is ida{" "}
+          <span className="text-red-700">(eye-duh)</span> and welcome to my
+          website hello my name is ida{" "}
+          <span className="text-red-700">(eye-duh)</span> and welcome to my
+          website hello my name is ida{" "}
+          <span className="text-red-700">(eye-duh)</span> and welcome to my
+          website hello my name is ida{" "}
+          <span className="text-red-700">(eye-duh)</span> and welcome to my
+          website hello my name is ida{" "}
+          <span className="text-red-700">(eye-duh)</span> and welcome to my
+          website hello my name is ida{" "}
+          <span className="text-red-700">(eye-duh)</span> and welcome to my
+          website hello my name is ida{" "}
+          <span className="text-red-700">(eye-duh)</span> and welcome to my
+          website hello my name is ida{" "}
+          <span className="text-red-700">(eye-duh)</span> and welcome to my
+          website hello my name is ida{" "}
+          <span className="text-red-700">(eye-duh)</span> and welcome to my
+          website hello my name is ida{" "}
+          <span className="text-red-700">(eye-duh)</span> and welcome to my
+          website hello my name is ida{" "}
+          <span className="text-red-700">(eye-duh)</span> and welcome to my
+          website hello my name is ida{" "}
+          <span className="text-red-700">(eye-duh)</span> and welcome to my
+          website hello my name is ida{" "}
+          <span className="text-red-700">(eye-duh)</span> and welcome to my
+          website hello my name is ida{" "}
+          <span className="text-red-700">(eye-duh)</span> and welcome to my
+          website hello my name is ida{" "}
+          <span className="text-red-700">(eye-duh)</span> and welcome to my
+          website hello my name is ida{" "}
+          <span className="text-red-700">(eye-duh)</span> and welcome to my
+          website
+        </p>
+      </div>
+    </>
   );
 };
 
