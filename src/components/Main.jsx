b657189f3300832b4cdd5883475db245630596d7
idakam <ida.kamali10@gmail.com>
2023-10-10 18:21:03 -0400

    new navbar layout

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
 import React from "react";
 import dynamic from "next/dynamic";
-import head from "../../public/assets/headlogo.png";
+import Link from "next/link";
+import head from "../../public/assets/face.png";
 // import ModelViewer from "../components/Three/ModelViewer";
 const ModelViewer = dynamic(import("../components/Three/ModelViewer"), {
   ssr: false,
@@ -12,7 +13,18 @@ const Main = () => {
   return (
     <>
       {/* <ModelViewer modelPath={"/assets/faceblend.glb"} /> */}
-      <div className="fixed mr-4 text-justify items-center justify-center ">
+
+      <div className="md:hidden flex flex-col h-full m-auto  w-full justify-center">
+        <Link className="flex" href="/">
+          <Image src={head} alt="/" />
+        </Link>
+        <h1 className="text-center uppercase"> hello </h1>
+        <h1 className="text-center uppercase"> my name</h1>
+        <h1 className="text-center uppercase"> is ida</h1>
+        <h1 className="text-center uppercase text-red-700">(eye-duh)</h1>
+      </div>
+
+      <div className="hidden md:fixed mr-4 text-justify items-center justify-center ">
         <p className="z-0 uppercase tracking-widest text-black overflow-y-hidden sm:text-sm md:text-base lg:text-lg xl:text-xl 2xl:text-2xl">
           hello my name is ida <span className="text-red-700">(eye-duh)</span>{" "}
           and welcome to my website hello my name is ida{" "}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,6 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import React, { useState } from "react";
 import { AiOutlineMenu, AiOutlineClose, AiOutlineMail } from "react-icons/ai";
-import logo from "../../public/assets/eyeduhlogo.png";
-// import head from "../../public/assets/headlogo.png";
 import head from "../../public/assets/face.png";
 
 import { FaGithub, FaLinkedinIn } from "react-icons/fa";
@@ -15,275 +13,124 @@ const Navbar = () => {
   const handleNav = () => {
     setMenuOpen(!menuOpen);
   };
-  // flex justify-center
+
+  const updateTime = () => {
+    let now = new Date();
+    const birth = new Date("June 20, 2023 19:47:00");
+
+    // setTimeout(() => {
+    //   const millis = Date.now() - start;
+
+    //   console.log(`seconds elapsed = ${Math.floor(millis / 1000)}`);
+    //   // Expected output: "seconds elapsed = 2"
+    // }, 2000);
+
+    // To calculate the time difference of two dates
+    let timeDifference = now.getTime() - birth.getTime();
+
+    // To calculate the no. of days between two dates
+    let dayDifference = Math.trunc(timeDifference / (1000 * 3600 * 24));
+
+    return dayDifference;
+  };
+
+  updateTime();
   return (
     <>
-      <nav className="hidden md:flex w-full h-screen overflow-scroll ">
-        <div className="flex h-screen flex-col w-full py-4 px-4">
-          <div className="px-5 flex justify-center m-auto hover:scale-105">
-            <Link className="" href="/">
-              <Image src={head} alt="/" />
+      <div className="hidden md:flex flex-col">
+        <nav className="bg-white bg-opacity-30">
+          <ul>
+            <Link href="/about">
+              <li
+                className={
+                  router.pathname == "/about"
+                    ? "ml- uppercase text-red-700 font-bold"
+                    : "ml- uppercase hover:text-red-700"
+                }
+              >
+                About
+              </li>
             </Link>
-          </div>
-
-          <div className="flex flex-col sm:text-lg md:text-lg lg:text-lg xl:text-xl 2xl:text-3xl px-[5%]">
-            <ul>
-              <Link href="/about">
+            <ul className="ml- uppercase py-2">
+              <Link href="/projects/beatdriver">
                 <li
                   className={
-                    router.pathname == "/about"
+                    router.pathname == "/projects/beatdriver"
                       ? "ml- uppercase text-red-700 font-bold"
                       : "ml- uppercase hover:text-red-700"
                   }
                 >
-                  About
+                  Beatdriver
                 </li>
               </Link>
-              <ul className="ml- uppercase py-2">
-                <span className="md:text-xs lg:text-sm xl:text-base 2xl:text-lg">
-                  Projects
-                </span>
-                <Link href="/projects/beatdriver">
-                  <li
-                    className={
-                      router.pathname == "/projects/beatdriver"
-                        ? "ml- uppercase text-red-700 font-bold"
-                        : "ml- uppercase hover:text-red-700"
-                    }
-                  >
-                    Beatdriver
-                  </li>
-                </Link>
-                <Link href="/projects/grooverecords">
-                  <li
-                    className={
-                      router.pathname == "/projects/grooverecords"
-                        ? "ml- uppercase text-red-700 font-bold"
-                        : "ml- uppercase hover:text-red-700"
-                    }
-                  >
-                    Groove Records
-                  </li>
-                </Link>
-                <Link href="/projects/posedetection">
-                  <li
-                    className={
-                      router.pathname == "/projects/posedetection"
-                        ? "ml- uppercase text-red-700 font-bold"
-                        : "ml- uppercase hover:text-red-700"
-                    }
-                  >
-                    3D Pose Detection
-                  </li>
-                </Link>
-                <Link href="/projects/viewfrommywindow">
-                  <li
-                    className={
-                      router.pathname == "/projects/viewfrommywindow"
-                        ? "ml- uppercase text-red-700 font-bold"
-                        : "ml- uppercase hover:text-red-700"
-                    }
-                  >
-                    View From My Window
-                  </li>
-                </Link>
-              </ul>
-              <ul className="ml- uppercase py-2">
-                <span className="md:text-xs lg:text-sm xl:text-base 2xl:text-lg">
-                  MISC
-                </span>
-                <Link href="/misc/painting">
-                  <li
-                    className={
-                      router.pathname == "/misc/painting"
-                        ? "ml- uppercase text-red-700 font-bold"
-                        : "ml- uppercase hover:text-red-700"
-                    }
-                  >
-                    Painting
-                  </li>
-                </Link>
-                <Link href="/misc/design">
-                  <li
-                    className={
-                      router.pathname == "/misc/design"
-                        ? "ml- uppercase text-red-700 font-bold"
-                        : "ml- uppercase hover:text-red-700"
-                    }
-                  >
-                    Video + Design
-                  </li>
-                </Link>
-              </ul>
-            </ul>
-            <div className="flex items-center justify-around w-full">
-              <div className="rounded-full shadow-xl shadow-gray-400 p-2 cursor-pointer hover:scale-105 ease-in duration-300">
-                <a
-                  target="_blank"
-                  href="https://www.linkedin.com/in/ida-kam/"
-                  rel="noopener noreferrer"
-                >
-                  <FaLinkedinIn />
-                </a>
-              </div>
-              <div className="rounded-full shadow-xl shadow-gray-400 p-2 cursor-pointer hover:scale-105 ease-in duration-300">
-                <a
-                  target="_blank"
-                  href="https://github.com/idakam"
-                  rel="noopener noreferrer"
-                >
-                  <FaGithub />
-                </a>
-              </div>
-              <div className="rounded-full shadow-xl shadow-gray-400 p-2 cursor-pointer hover:scale-105 ease-in duration-300">
-                <a
-                  target="_blank"
-                  href="mailto:ida.kamali10@gmail.com"
-                  rel="noopener noreferrer"
-                >
-                  <AiOutlineMail />
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </nav>
-      <div onClick={handleNav} className="flex md:hidden cursor-pointer w-full">
-        {" "}
-        <AiOutlineMenu size={40} />
-      </div>
-      {/* bg-[url('../../public/assets/background.png')] */}
-      <div
-        className={
-          menuOpen
-            ? "fixed flex-col left-0 top-0 w-full md:hidden h-screen ease-in duration-500 z-40 bg-cover bg-[url('../../public/assets/background.png')]"
-            : "hidden"
-        }
-      >
-        <div className="flex flex-col h-screen py-4 px-4 overflow-auto text-lg">
-          <div
-            onClick={handleNav}
-            className="fixed right-3 cursor-pointer z-40"
-          >
-            {""}
-            <AiOutlineClose size={40} />
-          </div>
-          <div className="flex justify-center w-auto h-auto m-auto">
-            <Link onClick={handleNav} className="" href="/">
-              <Image src={head} alt="/" />
-            </Link>
-          </div>
-
-          <div className="flex-1 text-5xl flex-col h-auto w-auto px-[5%]">
-            <ul>
-              <Link href="/about">
+              <Link href="/projects/grooverecords">
                 <li
-                  onClick={handleNav}
                   className={
-                    router.pathname == "/about"
-                      ? "ml- uppercase text-red-700"
-                      : "ml- uppercase"
+                    router.pathname == "/projects/grooverecords"
+                      ? "ml- uppercase text-red-700 font-bold"
+                      : "ml- uppercase hover:text-red-700"
                   }
                 >
-                  About
+                  Groove Records
                 </li>
               </Link>
-              <ul className="ml- uppercase py-2">
-                <span className="text-3xl">Projects</span>
-                <Link href="/projects/beatdriver">
-                  <li
-                    onClick={handleNav}
-                    className={
-                      router.pathname == "/projects/beatdriver"
-                        ? "ml- uppercase text-red-700"
-                        : "ml- uppercase"
-                    }
-                  >
-                    Beatdriver
-                  </li>
-                </Link>
-                <Link href="/projects/grooverecords">
-                  <li
-                    onClick={handleNav}
-                    className={
-                      router.pathname == "/projects/grooverecords"
-                        ? "ml- uppercase text-red-700 font-bold"
-                        : "ml- uppercase"
-                    }
-                  >
-                    Groove Records
-                  </li>
-                </Link>
-                <Link href="/projects/posedetection">
-                  <li
-                    onClick={handleNav}
-                    className={
-                      router.pathname == "/projects/posedetection"
-                        ? "ml- uppercase text-red-700 font-bold"
-                        : "ml- uppercase"
-                    }
-                  >
-                    3D Pose Detection
-                  </li>
-                </Link>
-                <Link onClick={handleNav} href="/projects/viewfrommywindow">
-                  <li
-                    className={
-                      router.pathname == "/projects/viewfrommywindow"
-                        ? "ml- uppercase text-red-700 font-bold"
-                        : "ml- uppercase"
-                    }
-                  >
-                    View From My Window
-                  </li>
-                </Link>
-              </ul>
-              <ul className="ml- uppercase py-2">
-                <span className="text-3xl">MISC</span>
-                <Link href="/misc/painting">
-                  <li
-                    onClick={handleNav}
-                    className={
-                      router.pathname == "/misc/painting"
-                        ? "ml- uppercase text-red-700 font-bold"
-                        : "ml- uppercase"
-                    }
-                  >
-                    Painting
-                  </li>
-                </Link>
-                <Link onClick={handleNav} href="/misc/design">
-                  <li
-                    className={
-                      router.pathname == "/misc/design"
-                        ? "ml- uppercase text-red-700 font-bold"
-                        : "ml- uppercase"
-                    }
-                  >
-                    Video + Design
-                  </li>
-                </Link>
-              </ul>
-            </ul>
-            <div className="flex items-center justify-around w-full">
-              <div className="rounded-full shadow-xl shadow-gray-400 p-2 cursor-pointer hover:scale-105 ease-in duration-300">
-                <a
-                  target="_blank"
-                  href="https://www.linkedin.com/in/ida-kam/"
-                  rel="noopener noreferrer"
+              <Link href="/projects/posedetection">
+                <li
+                  className={
+                    router.pathname == "/projects/posedetection"
+                      ? "ml- uppercase text-red-700 font-bold"
+                      : "ml- uppercase hover:text-red-700"
+                  }
                 >
-                  <FaLinkedinIn />
-                </a>
-              </div>
-
-              <div className="rounded-full shadow-xl shadow-gray-400 p-2 cursor-pointer hover:scale-105 ease-in duration-300">
+                  3D Pose Detection
+                </li>
+              </Link>
+              <Link href="/projects/viewfrommywindow">
+                <li
+                  className={
+                    router.pathname == "/projects/viewfrommywindow"
+                      ? "ml- uppercase text-red-700 font-bold"
+                      : "ml- uppercase hover:text-red-700"
+                  }
+                >
+                  View From My Window
+                </li>
+              </Link>
+            </ul>
+          </ul>
+          <div className="flex justify-around">
+            <div className="rounded-full shadow-xl shadow-gray-400 p-2 cursor-pointer hover:scale-105 ease-in duration-300">
+              <a
+                target="_blank"
+                href="https://www.linkedin.com/in/ida-kam/"
+                rel="noopener noreferrer"
+              >
+                <FaLinkedinIn />
+              </a>
+            </div>
+            <div className="rounded-full shadow-xl shadow-gray-400 p-2 cursor-pointer hover:scale-105 ease-in duration-300">
+              <a
+                target="_blank"
+                href="https://github.com/idakam"
+                rel="noopener noreferrer"
+              >
                 <FaGithub />
-              </div>
-              <div className="rounded-full shadow-xl shadow-gray-400 p-2 cursor-pointer hover:scale-105 ease-in duration-300">
+              </a>
+            </div>
+            <div className="rounded-full shadow-xl shadow-gray-400 p-2 cursor-pointer hover:scale-105 ease-in duration-300">
+              <a
+                target="_blank"
+                href="mailto:ida.kamali10@gmail.com"
+                rel="noopener noreferrer"
+              >
                 <AiOutlineMail />
-              </div>
+              </a>
             </div>
           </div>
+        </nav>
+        <div className="bg-white bg-opacity-30">
+          This website was born <br></br>
+          {updateTime()} days ago.
         </div>
       </div>
     </>
