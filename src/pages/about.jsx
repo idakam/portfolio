55029dd3cf4eab0ce4d0d028f2c0a1fa4e02091c
idakam <ida.kamali10@gmail.com>
2023-10-24 15:01:23 -0400

    fix bottom padding

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -8,7 +8,15 @@ const About = () => {
   return (
     <Project
       projectImg={selfie}
-      p1="Ida Kamali is a full-stack software engineer and artist with a passion for creating immersive and visually engaging applications. They are in pursuit of experiences centered around music, web design, community, and computational experimentation. When not coding, they are either listening to music, baking bread or painting. "
+      p1={
+        <p>
+          Ida Kamali is a full-stack software engineer and artist with a passion
+          for creating immersive and visually engaging applications. They are in
+          pursuit of experiences centered around music, web design, community,
+          and computational experimentation. When not coding, they are either
+          listening to music, baking bread or painting.
+        </p>
+      }
     />
   );
 };
