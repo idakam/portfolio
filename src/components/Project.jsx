1db2b6b059e4c324a2999265e214a6e87142bb57
idakam <ida.kamali10@gmail.com>
2023-11-08 15:55:09 -0500

    smaller images

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -19,14 +19,38 @@ const Project = ({
       exit={{ opacity: 0, y: 20 }}
       className="flex flex-col md:flex-row h-full"
     >
-      <div className="flex flex-col md:mr-[5%] justify-center md:w-1/2">
+      <div className="flex flex-col md:mr-[5%] justify-center md:w-1/2 h-auto group opacity-85">
         <Image
-          className="object-contain  md:max-h-full min-w-0 pb-3 md:pb-0"
+          className="object-contain  md:max-h-full min-w-0 pb-3 md:pb-0 group-hover:opacity-20 "
           src={projectImg}
           alt="/"
           priority="true"
           sizes="100vw"
-        />
+          width={600}
+          height={450}
+        ></Image>
+        <div className="hidden group-hover:block absolute top-[50%] left-[38%] translate-x-[-50%] translate-y-[-50%]">
+          {deployed ? (
+            <a target="_blank" href={deployed} rel="noopener noreferrer">
+              <p
+                target="_blank"
+                className="text-center py-3 font-bold text-lg cursor-pointer hover:scale-105"
+              >
+                Deployed
+              </p>
+            </a>
+          ) : null}
+          {github ? (
+            <a target="_blank" href={github} rel="noopener noreferrer">
+              <p
+                target="_blank"
+                className="text-center py-3 font-bold text-lg cursor-pointer hover:scale-105"
+              >
+                Github
+              </p>
+            </a>
+          ) : null}
+        </div>
       </div>
       <div className="bg-white bg-opacity-30 p-2 overflow-auto shadow-xl md:w-1/2 h-1/2s">
         {p1}
